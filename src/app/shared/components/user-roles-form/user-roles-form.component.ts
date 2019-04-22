1a7442187edd7ac68bce40bbca4bc02c8612da13
TheRayOfSeasons <sison.ray@ciit.edu.ph>
2019-04-22 16:38:20 +0800

    DL-0008-41 - User Management: Add validation for User's Role Information - Connected required data for each select box's options

--- src/app/shared/components/user-roles-form/user-roles-form.component.ts
+++ src/app/shared/components/user-roles-form/user-roles-form.component.ts
@@ -6,10 +6,17 @@ import { Constants } from 'app/shared/constants';
 import { MatSlideToggleModule, MatCheckboxModule } from '@angular/material';
 // import { SharedModule } from '@shared/shared.module';
 import {
+  CSOGroupService,
+  FormService,
+  MunicipalityService,
+  ProgramAccessLevelService,
+  ProgramService,
+  ProvinceService,
+  RegionService,
+  UserService,
   UserProgramRole,
   UserProgramRoleService,
   UserRoleService,
-  FormService,
   Page,
   Form
 } from 'app/core';
@@ -25,52 +32,109 @@ export class UserRolesFormComponent implements OnInit, OnChanges {
 
   userRolesForm: FormGroup;
   public page = new Page();
-  public rows = new Array<UserProgramRole>();
+  // public rows = new Array<UserProgramRole>();
+
+  // public labels = ['Role', 'Access Level', 'CSO Group', 'Location', 'Status', 'Actions'];
+  public roles;
+  public csoGroups;
+  public levels;
+  public municipalities;
+  public provinces;
+  public regions;
 
   constructor(
     public fb: FormBuilder,
+    public csoGroupService: CSOGroupService,
+    public municipalityService: MunicipalityService,
+    public programAccessLevelService: ProgramAccessLevelService,
+    public programService: ProgramService,
+    public provinceService: ProvinceService,
+    public regionService: RegionService,
+    public userService: UserService,
     public userProgramRoleService: UserProgramRoleService,
     public userRoleService: UserRoleService,
     private formService: FormService,
   ) {
     this.userRolesForm = fb.group({
-      programRoles: this.fb.group({
-        role: new FormControl('', [Validators.required]),
-        access: new FormControl('', [Validators.required]),
-        cso: new FormControl('', [Validators.required]),
-        region: new FormControl('', [Validators.required]),
-        province: new FormControl('', [Validators.required]),
-        municipality: new FormControl('', [Validators.required])
-      }),
+      role: new FormControl('', [Validators.required]),
+      access: new FormControl('', [Validators.required]),
+      cso: new FormControl('', [Validators.required]),
+      region: new FormControl('', [Validators.required]),
+      province: new FormControl('', [Validators.required]),
+      municipality: new FormControl('', [Validators.required])
     });
     // this.page.pageNumber = Constants.DEFAULT_PAGE_NUMBER;
     // this.page.size = Constants.DEFAULT_PAGE_SIZE;
   }
 
   ngOnInit() {
-    if (this.userRolesData) { 
+    if (this.userRolesData) {
       this.setUserRolesForm(this.userRolesData);
     }
-
-    this.rows = [
-      { role: 'Project Admin', level: 'National', csoGroup: 'N/A', region: 'N/A', isActive: true, objectId: '',
-      program: '', user: '',  province: '', municipality: '', barangay: '', district: 1, selected: false, assign(){} },
-      { role: 'CSO Admin', level: 'Regional', csoGroup: 'MAPAD', region: 'Region I', isActive: false, objectId: '',
-      program: '', user: '',  province: '', municipality: '', barangay: '', district: 1, selected: false, assign() {}}
-    ];
-    console.log(this.userRoleService.getAllUserRoles());
-    console.log(this.rows);
+    this.setDropdowns();
   }
 
   ngOnChanges (changes) {
+    this.setDropdowns();
   }
 
-  setUserRolesForm (data) {
+  setUserRolesForm(data) {
     Object.keys(data).map(key => {
       if (this.userRolesForm.controls[key]) {
         this.userRolesForm.controls[key].patchValue(data[key]);
       }
     });
+  }
+
+  setDropdowns() {
+    this.allUserRoles();
+    this.allAccessLevels();
+    this.allCSOGroups();
+    this.allRegions();
+    this.allProvinces();
+    this.allMunicipalities();
+  }
+
+  async allCSOGroups() {
+    const csoList: any = await this.csoGroupService.getAllCSOGroup().then(data => data);
+    console.log(csoList);
+    this.csoGroups = csoList;
+    return csoList;
+  }
+
+  async allMunicipalities() {
+    const municipalityList: any = await this.municipalityService.getAll().then(data => data);
+    console.log(municipalityList);
+    this.municipalities = municipalityList;
+    return municipalityList;
+  }
+
+  async allProvinces() {
+    const provinceList: any = await this.provinceService.getAll().then(data => data);
+    console.log(provinceList);
+    this.provinces = provinceList;
+    return provinceList;
+  }
+
+  async allRegions() {
+    const regionList: any = await this.regionService.getAll().then(data => data);
+    console.log(regionList);
+    this.regions = regionList;
+    return regionList;
+  }
+
+  async allUserRoles() {
+    const roleList: any = await this.userRoleService.getAllUserRoles().then(data => data);
+    console.log(roleList);
+    this.roles = roleList;
+    return roleList;
+  }
+
+  async allAccessLevels() {
+    const levelList: any = await this.programAccessLevelService.getAll().then(data => data);
+    console.log(levelList);
+    this.levels = levelList;
+    return levelList;
   }
 
   onFieldChange() {
@@ -79,8 +143,6 @@ export class UserRolesFormComponent implements OnInit, OnChanges {
 
   trimFormValues() {
     // const values = {};
-
-    
   }
 
 }
